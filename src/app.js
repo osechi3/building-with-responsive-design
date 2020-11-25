6446d1fe3df53edb8bc30f5e66f7a63f6bda0e46
osechi3 <holypoetwrk@gmail.com>
2020-11-25 16:56:26 +0300

    Creating the first part of the navigation bar

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
 import './styles/app.css'
 import 'normalize.css'
+
+class NavigationBar {
+  constructor () {
+    this.nestedListItems = document.querySelectorAll('.nav-bar__nested-list-item')
+
+    this.initListeners()
+  }
+
+  initListeners () {
+    this.nestedListItems.forEach(list => {
+      list.addEventListener('mouseenter', this.displaySubMenu.bind(this))
+      list.addEventListener('mouseleave', this.hideSubMenu.bind(this))
+    })
+  }
+
+  displaySubMenu (event) {
+    const subList =
+      event.currentTarget.querySelector('.nav-bar__sub-list')
+    subList.classList.remove('nav-bar__sub-list_hidden')
+  }
+
+  hideSubMenu (event) {
+    const subList =
+      event.currentTarget.querySelector('.nav-bar__sub-list')
+    subList.classList.add('nav-bar__sub-list_hidden')
+  }
+}
+
+// eslint-disable-next-line no-new
+new NavigationBar()
